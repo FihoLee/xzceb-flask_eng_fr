675de2f69af0c887668e4920afb32365633f3d8c
FihoLee <86739498+FihoLee@users.noreply.github.com>
2022-11-27 17:49:59 -0600

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -16,18 +16,20 @@ language_translator = LanguageTranslatorV3(
 )
 language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
-    frenchText=language_translator.translate(
-        text=englishText,
-        model_id='en-fr'
-        ).get_result()
 
+def english_To_French(englishText):
+    englishText = "Hello madame."
+    if(englishText==""):
+        frenchText = "No input (null) detected."
+    else:
+        x = language_translator.translate(englishText, model_id='en-fr').get_result()
+        frenchText = (x["translations"][0]["translation"])
     return frenchText
 
-def frenchToEnglish(frenchText):
-    englishText=language_translator.translate(
-        text=frenchText,
-        model_id='fr-en'
-        ).get_result()
-    
+def french_To_English(frenchText):
+    if(frenchText==""):
+        englishText = "No input (null) detected."
+    else:
+        x2 = language_translator.translate(frenchText, model_id='fr-en').get_result()
+        englishText = (x2["translations"][0]["translation"])
     return englishText
