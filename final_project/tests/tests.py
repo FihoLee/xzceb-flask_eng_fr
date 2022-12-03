6924b76ef2724e01cd407608fd7288f4bcad60a0
FihoLee <86739498+FihoLee@users.noreply.github.com>
2022-12-02 19:22:55 -0600

    Update tests.py

--- final_project/tests/tests.py
+++ final_project/tests/tests.py
@@ -1,1 +1,17 @@
+import unittest
+from translator import english_To_French, french_To_English
 
+class TestMyModule(unittest.TestCase):
+
+    def test_english_To_French(self):
+        self.assertNotEqual(english_To_French("Null"), "")
+    def test_english_To_French2(self):
+        self.assertEqual(english_To_French('Hello'), 'Bonjour')
+
+    def test_french_To_English(self):
+        self.assertNotEqual(french_To_English("Null"), "")
+    def test_french_To_English2(self):
+        self.assertEqual(french_To_English('Bonjour'), 'Hello')
+
+if __name__=='__main__':
+    unittest.main()
